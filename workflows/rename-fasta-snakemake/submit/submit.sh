7f6cd033fda9560d3f8e9f246815fae63fb60568
akrinos <akrinos@vt.edu>
2021-12-07 15:27:19 -0500

    adding job flag

--- workflows/rename-fasta-snakemake/submit/submit.sh
+++ workflows/rename-fasta-snakemake/submit/submit.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #SBATCH --time=500
 
-snakemake --cores all --cluster-config cluster.yaml "sbatch -A {cluster.account} -t {cluster.time} -p {cluster.queue} -N {cluster.nodes} --cores {cluster.cpupertask}"
+snakemake --jobs 10 --cores all --cluster-config cluster.yaml --cluster "sbatch -A {cluster.account} -t {cluster.time} -p {cluster.queue} -N {cluster.nodes} --cores {cluster.cpupertask}"
