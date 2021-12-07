a936358a508d61218d9d6309f58b7e57a3a598ba
akrinos <akrinos@vt.edu>
2021-12-07 15:50:03 -0500

    Brute forcing for demo

--- workflows/rename-fasta-snakemake/submit/submit.sh
+++ workflows/rename-fasta-snakemake/submit/submit.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #SBATCH --time=500
+#SBATCH -p scavenger
 
-snakemake --jobs 10 --cores all --cluster-config cluster.yaml --cluster "sbatch -A {cluster.account} -t {cluster.time} -p {cluster.queue} -N {cluster.nodes} --cores {cluster.cpupertask}"
+snakemake --use-conda --jobs 10 --cores all --cluster-config cluster.yaml --cluster "sbatch --parsable -t 50 -p scavenger -N 1 --cores 5"
