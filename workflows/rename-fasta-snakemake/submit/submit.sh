cb737dcf91681a614381cb06e762ef7214ca21be
akrinos <akrinos@vt.edu>
2021-12-07 15:18:24 -0500

    updating to use parallelization if available

--- workflows/rename-fasta-snakemake/submit/submit.sh
+++ workflows/rename-fasta-snakemake/submit/submit.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #SBATCH --time=500
 
-snakemake --cores 1 --cluster-config cluster.yaml "sbatch -A {cluster.account} -t {cluster.time} -p {cluster.queue} -N {cluster.nodes} --cores {cluster.cpupertask}"
+snakemake --cores all --cluster-config cluster.yaml "sbatch -A {cluster.account} -t {cluster.time} -p {cluster.queue} -N {cluster.nodes} --cores {cluster.cpupertask}"
